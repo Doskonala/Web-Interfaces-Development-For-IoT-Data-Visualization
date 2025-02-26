f475c13b2ff95f5ce5fedde1a0d7e00fe1c250b4
Nataliia Kashuba <36890358-nataliiakashub1@users.noreply.replit.com>
2025-02-26 15:20:27 +0000

    lab1

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+function ClickButton(){
+	function showMessage(){
+		alert("Hello World");
+	}
+	return <button onClick={showMessage}>Click me</button>;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
+		<ClickButton/>
 	</React.StrictMode>
 )
